31359b1d88277497eedc238f3b18a8cfca7e55ca
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-08-15 03:14:15 -0500

    ADD: search component

--- src/components/search/SearchScreen/SearchScreen.tsx
+++ src/components/search/SearchScreen/SearchScreen.tsx
@@ -1,9 +1,11 @@
 import queryString from "query-string";
 import { useMemo } from "react";
 import { RouteComponentProps, useLocation } from "react-router-dom";
+import { heroes } from "../../../data/heroes";
 import { useForm } from "../../../hooks/useForm";
 import { getHeroSearch } from "../../../selectors/getHeroSearch";
 import HeroCard from "../../heroes/HeroCard/HeroCard";
+import Container from "../../utils/Container/Container";
 
 interface MyProps extends RouteComponentProps<any> {}
 const defaultProps = {};
@@ -25,19 +27,21 @@ const SearchScreen = (props: MyProps) => {
     e.preventDefault();
     history.push(`?q=${search}`);
   };
+  console.log(heroes.length);
 
   return (
-    <div data-testid="SearchScreen" className="row justify-content-center">
-      <div className="col-md-10">
-        <h1>SearchScreen component</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2">
-          <div className="">
+    <div data-testid="SearchScreen" className="p-4">
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        <div className="text-white">
+          <Container>
             <form onSubmit={handlerSubmit}>
-              <div className="form-group">
-                <label htmlFor="search">Busca un Heroe</label>
+              <div className="flex flex-col my-2">
+                <label htmlFor="search" className="text-base font-bold">
+                  Busca un Heroe
+                </label>
                 <input
                   type="text"
-                  className="form-control"
+                  className="p-2 my-2 border-none rounded-lg outline-none bg-heroBlack text-heroWhite"
                   name="search"
                   id="search"
                   value={search}
@@ -45,22 +49,36 @@ const SearchScreen = (props: MyProps) => {
                   onChange={handlerChange}
                   placeholder="Ingresa una busqueda"
                 />
-                <button type="submit">Buscar</button>
+                <button
+                  type="submit"
+                  className="w-full px-3 py-2 my-2 font-bold duration-75 ease-out delay-75 transform scale-100 bg-green-600 rounded-full active:scale-75 active:bg-green-800 "
+                >
+                  Buscar
+                </button>
               </div>
             </form>
-          </div>
-          <div className="">
-            <h2>Results</h2>
-            {heroesFiltered.map((hero) => (
-              <HeroCard key={hero.id} heroe={hero} />
-            ))}
-
-            {/* falta agregar validacion de cuando no hay resultados y cuando no encuentra la busqueda */}
-          </div>
+          </Container>
+        </div>
+        <div className="col-span-2 p-2 mx-4">
+          {heroesFiltered.length !== 0 ? (
+            <>
+              <h2 className="my-4 text-4xl font-bold text-center">
+                {q === "" ? `All Heroes` : `Results with ${q}`}
+              </h2>
+              <div className="grid grid-cols-1 grid-rows-1 gap-6 md:grid-cols-2 lg:grid-cols-2">
+                {heroesFiltered.map((hero) => (
+                  <HeroCard key={hero.id} heroe={hero} />
+                ))}
+              </div>
+            </>
+          ) : (
+            <h2 className="my-4 text-4xl font-bold text-center">
+              There are no heroes with "{q}"
+            </h2>
+          )}
         </div>
       </div>
     </div>
   );
 };
-
 export default SearchScreen;
